3000726ea11fb8f0069a940680882d40d80a450c
Kevin <kevin.thai21@gmail.com>
2020-08-01 11:27:11 -0700

    Extension can go to links now.

--- background.js
+++ background.js
@@ -7,6 +7,9 @@ var test = document.getElementById("transferButton"); // checks the button press
 var input = document.getElementById("yt-link"); // gets the input values
 var link;
 
+var mvName=[];
+var channelName=[];
+
 // if the button is not Null, then checks if the button has been pressed.
 if(test) {
     test.onclick = function() { definePlaylist(input.value)};
@@ -14,6 +17,41 @@ if(test) {
 
 /* definePlaylist() gets the link and saves the link */
 function definePlaylist(ytLink) {
-    link = ytLink;
+    // const init = "https://";
+    // link = "";
+    // // if the link provided doesn't have the "https", add it.
+    // if (!link.includes(init)) {
+    //     link = link.concat(init,ytLink);
+    // }
+    // else {
+    //     link = ytLink;
+    //     console.log(link);
+    // }
+
+
+    link = ytLink
+
+    // start the transfer process.
+    transferPlaylist();
     alert(link);
+}
+
+function transferPlaylist() {
+    chrome.tabs.create({ url : link});
+    // var win = window.open(link, "_blank");
+    // win.focus();
+
+    getMusic();
+}
+
+function getMusic() {
+    mvName.push("SOLO");
+    channelName.push("JENNIE - Topic");
+    alert(mvName);
+    alert(channelName);
+    chrome.tabs.create({ url : "https://www.google.com"});
+}
+
+function done() {
+    alert("Done transferring playlist!");
 }
